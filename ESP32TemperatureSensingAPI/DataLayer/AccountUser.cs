61c7142cd65ace9aef315a644f74c0566f6a5d10
Carl du Preez <carl.dupreez07@gmail.com>
2021-02-23 08:52:12 +0200

    Working on the design and setting up the project template

---
+++ ESP32TemperatureSensingAPI/DataLayer/AccountUser.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DataLayer
+{
+    public class AccountUser
+    {
+
+    }
+}
---
+++ ESP32TemperatureSensingAPI/ESP32TemperatureSensingAPI/Controllers/AccountUserController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ESP32TemperatureSensingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountUserController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
